b6e34862e0b1c5964c074d1c55ba70a307e6f089
柚子茶 <yansheyang@icloud.com>
2024-08-08 09:01:35 +0800

    ALL
    ALL

---
+++ myself/lesson1/lesson1/MDK-ARM/lesson1/gpio.d
@@ -0,0 +1,29 @@
+lesson1/gpio.o: ..\Core\Src\gpio.c ..\Core\Inc\gpio.h ..\Core\Inc\main.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_ll_system.h \
+  ..\Drivers\CMSIS\Device\ST\STM32C0xx\Include\stm32c0xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32C0xx\Include\stm32c031xx.h \
+  ..\Drivers\CMSIS\Include\core_cm0plus.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Users\hunxi\Documents\GitHub\Modern-Embedded-Systems-Programming\myself\lesson1\lesson1\Drivers\CMSIS\Include\cmsis_version.h \
+  C:\Users\hunxi\Documents\GitHub\Modern-Embedded-Systems-Programming\myself\lesson1\lesson1\Drivers\CMSIS\Include\cmsis_compiler.h \
+  C:\Users\hunxi\Documents\GitHub\Modern-Embedded-Systems-Programming\myself\lesson1\lesson1\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Users\hunxi\Documents\GitHub\Modern-Embedded-Systems-Programming\myself\lesson1\lesson1\Drivers\CMSIS\Include\mpu_armv7.h \
+  ..\Drivers\CMSIS\Device\ST\STM32C0xx\Include\system_stm32c0xx.h \
+  ..\Core\Inc\stm32c0xx_hal_conf.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_rcc.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_def.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_ll_rcc.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_rcc_ex.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_gpio.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_gpio_ex.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_dma.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_dma_ex.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_cortex.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_exti.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_flash.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_flash_ex.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_pwr.h \
+  ..\Drivers\STM32C0xx_HAL_Driver\Inc\stm32c0xx_hal_pwr_ex.h
---
+++ myself/lesson1/lesson1/MDK-ARM/lesson1/startup_stm32c031xx.d
@@ -0,0 +1,1 @@
+lesson1\startup_stm32c031xx.o: startup_stm32c031xx.s
