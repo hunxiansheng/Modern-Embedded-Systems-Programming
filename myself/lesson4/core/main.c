d0632ae4421e4f8e19d553eb225fb92299682db9
yansheyang <hunxiansheng@gmail.com>
2024-08-09 17:27:14 +0800

    ALL

--- myself/lesson4/core/main.c
+++ myself/lesson4/core/main.c
@@ -1,16 +1,28 @@
 extern void SystemInit(void);
-int counter = 0;
+
 int main(void)
 {
-    int *p_int;
-    p_int = &counter;
-    while (*p_int < 21)
-    {
-        ++(*p_int); 
-    }
-		p_int = (int *)0x20000062U;
-		*p_int = 0xDEADBEFF;
-    return 0;
+    // enable GPIOA clock port for the LED LD4
+    *((unsigned int *)0x40021034U) = 0x001U; // RCC IOPENR register
+	  *((unsigned int *)0x50000000U) = 0x400U; // GPIOA MODER register
+    *((unsigned int *)0x50000004U) = 0x000U; // GPIOA OTYPER register
+		*((unsigned int *)0x50000008U) = 0x400U; // GPIOA OSPEEDR register
+		*((unsigned int *)0x5000000CU) = 0x000U; // GPIOA PUPDR register
+		while(1)
+		{
+			 *((unsigned int *)0x50000018U) = 0x20U; // GPIOA BSRR register led on
+			//delay
+			  int volatile counter = 0;
+        while (counter < 500000) {  // delay loop
+            ++counter;
+        }
+			 *((unsigned int *)0x50000018U) = 0x200000U; // GPIOA BSRR register led off
+			//delay
+				counter = 0;
+        while (counter < 500000) {  // delay loop
+            ++counter;
+        }
+		}
 }
 
 void SystemInit(void)
